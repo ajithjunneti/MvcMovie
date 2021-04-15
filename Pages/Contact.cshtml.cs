accabfdafd9f359dc4712e898643936bee837357
ajithjunneti <junnetiajith@gmail.com>
2021-04-15 19:31:06 -0400

    added new page model

---
+++ Pages/Contact.cshtml.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace MvcMovie.Pages
+{
+    public class ContactModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public ContactModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToShortDateString();
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+}
